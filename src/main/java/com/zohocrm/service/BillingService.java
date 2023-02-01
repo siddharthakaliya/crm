30e1d4c899bf22c184f4fd82397826cec1bdfaeb
Your Name <you@example.com>
2023-02-01 15:10:41 +0530

    zohocrmapp5 backup

---
+++ src/main/java/com/zohocrm/service/BillingService.java
@@ -0,0 +1,12 @@
+package com.zohocrm.service;
+
+import java.util.List;
+
+import com.zohocrm.entities.Billing;
+
+public interface BillingService {
+	public void generateBill(Billing bill);
+
+	List<Billing> getBilling();
+
+}
---
+++ src/main/java/com/zohocrm/service/ContactService.java
@@ -0,0 +1,14 @@
+package com.zohocrm.service;
+
+import java.util.List;
+
+import com.zohocrm.entities.Contact;
+
+public interface ContactService {
+	public void saveContact(Contact contact);
+
+	List<Contact> getContacts();
+
+	public Contact getContactById(long id);
+
+}
